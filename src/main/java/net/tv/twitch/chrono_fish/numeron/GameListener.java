dff4bd476661b6047e26b703e0427e837cc56059
Daka-fish <yamatohidaka24@gmail.com>
2024-09-19 21:55:36 +0900

    first commit

---
+++ src/main/java/net/tv/twitch/chrono_fish/numeron/GameListener.java
@@ -0,0 +1,2 @@
+package net.tv.twitch.chrono_fish.numeron;public class GameListener {
+}
---
+++ src/main/java/net/tv/twitch/chrono_fish/numeron/Numeron.java
@@ -0,0 +1,17 @@
+package net.tv.twitch.chrono_fish.numeron;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Numeron extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
